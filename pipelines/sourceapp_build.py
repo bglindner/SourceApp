07aae13afe042fc8227514350ac35a9998cc8745
Blake G Lindner <36962040+blindner6@users.noreply.github.com>
2024-02-15 11:20:08 -0500

    Update sourceapp_build.py

--- pipelines/sourceapp_build.py
+++ pipelines/sourceapp_build.py
@@ -182,7 +182,7 @@ def flag_crx(workdir):
     for cluster in singletons.index:
         df = df[~df.iloc[:,1].str.contains(cluster)]
     crx = df['genome']
-    sdf = pd.read_csv(workdir+ "/sources.txt",header=None,sep="\t")
+    sdf = pd.read_csv(workdir + "/sinfo.csv", sep=",",header=None) 
     for x in range(len(sdf.iloc[:,0])): # all genomes
         genome = sdf.iloc[x,0]
         if crx.str.contains(genome).any(): # genomes found to be concerning
